0c27991c281ca6517651c5473008ceb2a6d188a8
vitorsoaresferreira <vitorsoftwaredeveloper@gmail.com>
2025-11-20 10:10:57 -0300

    feat: implements .env for login

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,13 @@
+require("dotenv").config();
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "3oyhuw",
   e2e: {
+    env: {
+      API_USER: process.env.API_USER,
+      API_PASS: process.env.API_PASS,
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,31 +1,5 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
-Cypress.Commands.add("login", () => {
-  cy.visit("/aplicativos/apps");
+Cypress.Commands.add("loginCodex", () => {
+  cy.visit("https://core.hgtx.com.br/aplicativos/apps");
 
   cy.get("#loginInputEmail")
     .should("be.visible")
@@ -35,6 +9,22 @@ Cypress.Commands.add("login", () => {
     .should("be.visible")
     .should("not.be.disabled")
     .type("tester123");
+
+  cy.get('button[type="submit"]').should("be.enabled").click();
+});
+
+Cypress.Commands.add("loginCreci", () => {
+  const user = Cypress.env("API_USER");
+  const pass = Cypress.env("API_PASS");
+
+  cy.visit("https://creci-hgtxcore.goutron.com.br/aplicativos/apps");
+
+  cy.get("#loginInputEmail").should("be.visible").type(user);
+
+  cy.get("#mat-input-1")
+    .should("be.visible")
+    .should("not.be.disabled")
+    .type(pass);
 
   cy.get('button[type="submit"]').should("be.enabled").click();
 });
