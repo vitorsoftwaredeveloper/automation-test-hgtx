005b737e255befb8c76275f53a2f97ac61962222
vitorsoaresferreira <vitorsoftwaredeveloper@gmail.com>
2025-11-22 05:52:13 -0300

    fix: remove access credentials

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,14 +1,15 @@
 Cypress.Commands.add("loginCodex", () => {
+  const user = Cypress.env("API_USER");
+  const pass = Cypress.env("API_PASS");
+
   cy.visit("https://core.hgtx.com.br/aplicativos/apps");
 
-  cy.get("#loginInputEmail")
-    .should("be.visible")
-    .type("qatestercodex@hgtx.com.br");
+  cy.get("#loginInputEmail").should("be.visible").type(user);
 
   cy.get("#mat-input-1")
     .should("be.visible")
     .should("not.be.disabled")
-    .type("tester123");
+    .type(pass);
 
   cy.get('button[type="submit"]').should("be.enabled").click();
 });
